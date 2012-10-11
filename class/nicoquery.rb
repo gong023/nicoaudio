4f2601364bd9cca90976d8b819b81f606e10ccf1
gong023 <gon.gong.gone@gmail.com>
2012-10-11 23:16:15 +0900

    create table文（参考用）

--- class/nicoquery.rb
+++ class/nicoquery.rb
@@ -9,4 +9,18 @@ module NicoQuery
     "SELECT * FROM daily_music WHERE ctime between '#{from_date}' AND '#{to_date}'"
   end
 
+  def create_table table
+    table = 'daily_music' if table.nil?
+    "CREATE TABLE `#{table}` (
+      `id` bigint(20) unsigned NOT NULL auto_increment,
+      `video_id` varchar(30) NOT NULL,
+      `title` varchar(255) NOT NULL,
+      `ctime` timestamp NOT NULL default CURRENT_TIMESTAMP,
+      PRIMARY KEY  (`id`),
+      UNIQUE KEY `video_id` (`video_id`),
+      KEY `video_id_index` (`video_id`),
+      KEY `ctime_index` (`ctime`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=923 DEFAULT CHARSET=utf8"
+  end
+
 end
