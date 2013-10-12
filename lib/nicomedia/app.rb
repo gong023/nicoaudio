1cab219fdf153b7eef89e0b75e9d9086d1b2eb80
gong023 <gon.gong.gone@gmail.com>
2013-10-12 16:53:22 +0900

    ははは廃校で〜す

--- lib/nicomedia/app.rb
+++ lib/nicomedia/app.rb
@@ -1,71 +1,75 @@
 module NicoMedia
   class App
-    @record_history = Record::History.new
+    def initialize
+      @record_history = Record::History.new
+      @step_count = 0
+      self.class.create_step
+    end
 
-    class << self
-      def hourly
-        begin
-          @record_history.create_new Agent::Ranking.filtered "music"
-          Report::Normal.execute[method(:step_undownload)][method(:step_downloaded)][method(:step_convert)]
-        rescue => e
-          Report::Abnormal.execute e
-        ensure
-          validate_file
-        end
-      end
-
-      STEP = [
-          {
-              state: :undownload,
-              type: "video",
-              client: Agent::Video,
-              checker: System::File
-          },
-          {
-              state: :downloaded,
-              type: "audio",
-              client: System::Ffmpeg,
-              checker: System::File
-          },
-          {
-              state: :convert,
-              type: "audio",
-              client: System::S3,
-              checker: System::S3
-          }
-      ]
-
-      STEP.each_index do |c|
-        next_state = STEP.at(c + 1) ? STEP.at(c + 1)[:state] : :uploaded
-        define_method "step_#{STEP[c][:state]}".to_sym do
-          targets = @record_history.read_recently STEP[c][:state]
-          executer = STEP[c][:state] == :undownloaded ? self.method(:fire) : self.method(:threads_fire)
-          executer.call(targets) do |list|
-            video_id = list["video_id"]
-            STEP[c][:client].method(:exec).call(video_id)
-            @record_history.update_state([video_id], next_state) if STEP[c][:checker].method(:exist?).call(STEP[c][:type], video_id)
-          end
-        end
-      end
-
-      private
-      def validate_file
-        today = Schedule::Util.today
-        System::Directory.create_video_by_date today
-        System::Directory.create_audio_by_date today
-        @record_history.update_state(System::Find.mp4_by_date(today), :downloaded)
-        @record_history.update_state(System::Find.mp3_by_date(today), :convert)
-      end
-
-      def threads_fire list
-        threads = []
-        list.each { |l| threads << Thread.new() { yield(l) } }
-        threads.each { |t| t.join }
-      end
-
-      def fire list
-        list.each { |l| yield(l) }
+    def hourly
+      begin
+        @record_history.create_new Agent::Ranking.filtered "music"
+        Report::Normal.execute[method(:step_video)][method(:step_audio)][method(:step_s3)]
+      rescue => e
+        Report::Abnormal.execute e
+      ensure
+        validate_file
       end
     end
+
+    FILE_STATES = %i(registerd downloaded converted uploaded).freeze
+
+    STEP = {
+        video: {
+            type: "video",
+            client: Agent::Video,
+            checker: System::File
+        },
+        audio: {
+            type: "audio",
+            client: System::Ffmpeg,
+            checker: System::File
+        },
+        s3: {
+            type: "audio",
+            client: System::S3,
+            checker: System::S3
+        }
+    }
+
+    def self.create_step
+      STEP.each do |k, v|
+        define_method "step_#{k}" do
+          next_state = FILE_STATES[@step_count + 1]
+          targets = @record_history.read_recently FILE_STATES[@step_count]
+          target_executer = k == :video ? method(:fire) : method(:threads_fire)
+          target_executer.call(targets) do |list|
+            video_id = list["video_id"]
+            v[:client].method(:exec).call(video_id)
+            @record_history.update_state([video_id], next_state) if v[:checker].method(:exist?).call(v[:type], video_id)
+          end
+          @step_count += 1
+        end
+      end
+    end
+
+    private
+    def validate_file
+      today = Schedule::Util.today
+      System::Directory.create_video_by_date today
+      System::Directory.create_audio_by_date today
+      @record_history.update_state(System::Find.mp4_by_date(today), :downloaded)
+      @record_history.update_state(System::Find.mp3_by_date(today), :converted)
+    end
+
+    def threads_fire list
+      threads = []
+      list.each { |l| threads << Thread.new() { yield(l) } }
+      threads.each { |t| t.join }
+    end
+
+    def fire list
+        list.each { |l| yield(l) }
+      end
   end
 end
