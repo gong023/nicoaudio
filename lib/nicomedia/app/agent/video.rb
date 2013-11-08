0c5e26f35fdbd3aa6cc6976f0e3f9608e5dd853a
gong023 <gon.gong.gone@gmail.com>
2013-11-08 23:51:23 +0900

    ｽﾞｻ

--- lib/nicomedia/app/agent/video.rb
+++ lib/nicomedia/app/agent/video.rb
@@ -2,13 +2,17 @@ module NicoMedia
   class Agent
     class Video
       def self.exec video_id
-        System::File.create_mp4(video_id) { Agent.client.video(video_id).get_video }
-      rescue Net::HTTPClientError
+        System::File.create_mp4(video_id) { method(:get_video).call(video_id) }
+      rescue # XXX:fixme
         Record::History.instance.update_state(video_id, :invalid)
-        Log.write("video", "#{video_id} / client error", "fail")
-      rescue Net::HTTPServerError
+        Report::Log.write("video", "#{video_id} / error", "fail")
+      end
+
+      def self.get_video video_id
+        Agent.client.video(video_id).get_video
+      rescue # XXX:fixme Net::HTTPClientError, Net::HTTPServerError
         Record::History.instance.update_state(video_id, :invalid)
-        Log.write("video", "#{video_id} / server error", "fail")
+        Report::Log.write("video", "#{video_id} / error", "fail")
       end
     end
   end
