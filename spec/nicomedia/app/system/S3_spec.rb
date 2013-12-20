43912155225f44b7e7cd5cb8d8cf4099039e50c5
gong023 <gon.gong.gone@gmail.com>
2013-12-21 00:55:10 +0900

    アイカツ！アイカツ！

--- spec/nicomedia/app/system/S3_spec.rb
+++ spec/nicomedia/app/system/S3_spec.rb
@@ -1,40 +1,37 @@
 require 'spec_helper'
 
 describe NicoMedia::System::S3 do
-  subject { described_class }
   let(:sample_id) { "sm123" }
 
   describe "#define_remote_path" do
+    subject { described_class.define_remote_path(media) }
     context "with audio name" do
+      let(:media) { "#{sample_id}.mp3" }
       before do
         Record::History.instance.should_receive(:read_created_at).with("#{sample_id}.mp3").and_return("2013-01-01")
       end
-      it "return audio path" do
-        expect(subject.define_remote_path("#{sample_id}.mp3")).to eq "audio/2013-01-01"
-      end
+      it { expect(subject).to eq "audio/2013-01-01" }
     end
 
     context "with video name" do
+      let(:media) { "#{sample_id}.mp4" }
       before do
         Record::History.instance.should_receive(:read_created_at).with("#{sample_id}.mp4").and_return("2013-01-01")
       end
-      it "return video path" do
-        expect(subject.define_remote_path("#{sample_id}.mp4")).to eq "video/2013-01-01"
-      end
+      it { expect(subject).to eq "video/2013-01-01" }
     end
   end
 
   describe "#define_content_type" do
+    subject { described_class.define_content_type(media) }
     context "with audio name" do
-      it "return audio/mpeg" do
-        expect(subject.define_content_type("#{sample_id}.mp3")).to eq "audio/mpeg"
-      end
+      let(:media) { "#{sample_id}.mp3" }
+      it { expect(subject).to eq "audio/mpeg" }
     end
 
     context "with video name" do
-      it "return video/mp4" do
-        expect(subject.define_content_type("#{sample_id}.mp4")).to eq "video/mp4"
-      end
+      let(:media) { "#{sample_id}.mp4" }
+      it { expect(subject).to eq "video/mp4" }
     end
   end
 end
