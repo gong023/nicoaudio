9380be10d2d2fcc9cd6db6a726f87424aaf83434
gong023 <gon.gong.gone@gmail.com>
2014-01-04 20:34:58 +0900

    雑

--- spec/nicomedia/app/setting_spec.rb
+++ spec/nicomedia/app/setting_spec.rb
@@ -3,14 +3,14 @@ require "spec_helper"
 describe NicoMedia::Setting do
   describe ".file" do
     it "load test_setting.yaml under spec directory" do
-      expect(Setting.file).to match /test_setting\.yaml$/
+      expect(Setting.file).to match /test_setting\.yml$/
     end
   end
 
   describe "#method_missing" do
     context "with setting exist" do
       it "load setting dynamically" do
-        expected_setting = YAML.load_file(File.dirname(__FILE__)+"/../../../test_setting.yaml")
+        expected_setting = YAML.load_file(File.dirname(__FILE__)+"/../../../test_setting.yml")
 
         expect(Setting.nico).to eq expected_setting["nico"]
         expect(Setting.mysql).to eq expected_setting["mysql"]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,4 @@ $:.concat Dir.glob(File.dirname(File.dirname(__FILE__)) + "/lib/**/")
 require "nicomedia"
 include NicoMedia
 
-NicoMedia::Setting.file = File.dirname(__FILE__) + "/../test_setting.yaml"
+NicoMedia::Setting.file = File.dirname(__FILE__) + "/../test_setting.yml"
